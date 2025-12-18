9fd3cd9ddd0e0181d7eb0f65cde93f415a3c451e
Ashen Nethsara <ashennethsara02@gmail.com>
2025-12-19 02:35:34 +0530

    Router Update

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import cors from "cors";
 import { connectDB } from "./db.js";
 import userRouter from "./routers/userRouter.js";
 import feedbackRouter from "./routers/feedbackRouter.js";
+import productRouter from "./routers/productRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 
 
@@ -36,7 +38,10 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/users",userRouter)
-app.use("/api/feedback", feedbackRouter);
+app.use("/api/feedback", feedbackRouter)
+app.use("/api/orders",orderRouter)
+app.use("/api/products",productRouter)
+
 
 
 connectDB();
